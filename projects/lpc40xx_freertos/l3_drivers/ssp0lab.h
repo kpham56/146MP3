fbb724251638b1793b178f034d0d441182ee44c4
kpham56 <nonobobohoho@gmail.com>
2022-05-12 12:55:33 -0700

    lmao im bad at git

--- projects/lpc40xx_freertos/l3_drivers/ssp0lab.h
+++ projects/lpc40xx_freertos/l3_drivers/ssp0lab.h
@@ -4,6 +4,3 @@ void ssp0initialize(uint32_t max_clock_khz);
 void ssp0__set_max_clock(uint32_t max_clock_khz);
 uint8_t ssp0lab__exchange_byte(uint8_t data_out);
 
-void ssp1initialize(uint32_t max_clock_khz);
-void ssp1__set_max_clock(uint32_t max_clock_khz);
-uint16_t ssp1lab__exchange_byte(uint16_t data_out);
